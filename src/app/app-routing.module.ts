5e7690f95205eaac01d45b7f2003814926e6b80c
Detlef Abbing <78258248+KWTZ@users.noreply.github.com>
2024-02-27 08:00:28 +0100

    v1.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
+import { PlacementComponent } from './placement/placement.component';
 
 const routes: Routes = [
   { path: '',  component: IntroComponent, pathMatch: 'full' },
+  { path: 'placement',  component: PlacementComponent, pathMatch: 'full' },
   
 
 ];
