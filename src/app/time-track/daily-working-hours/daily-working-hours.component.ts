c1578087c774e331b906d45ad8de909bdb0b4f76
KWTZ <78258248+KWTZ@users.noreply.github.com>
2024-02-28 21:57:40 +0100

    v1.0.9

--- src/app/time-track/daily-working-hours/daily-working-hours.component.ts
+++ src/app/time-track/daily-working-hours/daily-working-hours.component.ts
@@ -8,19 +8,6 @@ import { DataService } from 'src/app/_services/data.service';
 })
 export class DailyWorkingHoursComponent implements OnInit {
 
-  // sqlString: string = 'SELECT @i:=@i+1 AS rowNo, ' +
-  //                       ' t.idAssignment, t.internalPOSNo ' +
-  //                       ', DATE_FORMAT(t.operationDate, "%d.%m.%Y") as operationDate ' +
-  //                       ', weekday(t.operationDate)+1 as weekday ' +
-  //                       ', Date_Format(t.begin, "%H:%m") as begin ' +
-  //                       ', Date_Format(t.end, "%H:%m") as end  ' +
-  //                       '  , t.posname, t.posStreet, t.postalcode, t.city ' +
-  //                       'FROM ' +
-  //                       '  (Select a.*,  p.externalPOSNo, p.posName, p.posStreet, p.postalcode, p.city from pos_assignments a ' +
-  //                       '  inner join pos_pointofsales p on a.internalPOSNo=p.internalPOSNo) AS t, ' +
-  //                       '  (SELECT @i:=0) AS foo ' +
-  //                       'where t.promoterNo="' + localStorage.getItem('promoterNo') + '"';
-
   sqlString: string = 'Select  a.idAssignment, a.internalPOSNo ' +
                         ' , p.externalPOSNo, p.posName, p.posStreet, p.postalcode, p.city ' +
                         ', DATE_FORMAT(a.operationDate, "%d.%m.%Y") as operationDate ' +
