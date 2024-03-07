2353dcd8354de00b148e7a504e303f76c73658fe
KWTZ <78258248+KWTZ@users.noreply.github.com>
2024-03-07 18:14:21 +0100

    V1.1.7

--- src/app/report/daily-report/daily-report.component.ts
+++ src/app/report/daily-report/daily-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { DataService } from 'src/app/_services/data.service';
 
 @Component({
   selector: 'app-daily-report',
@@ -8,27 +10,74 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DailyReportComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
+  sqlInsertReport = "INSERT INTO pos_reports(promoterNo, internalPOSNo, reportDate, frequency, leads, talks, testerQty, revenue, display, nameTag, tester, asm, img, comment, modified, modifiedBy, created, createdBy) VALUES "
+  isValid = false;
+  rep=Report;
+
+  promoterNo;
+  currentAssign;
+  dataStored=false;
+
+  constructor(private http:HttpClient,
+    private dataService: DataService) { }
 
   ngOnInit(): void {
+    let promoter = JSON.parse(localStorage.getItem("promoter"));
+    this.promoterNo=promoter['promoterNo']
+    this.currentAssign = JSON.parse(localStorage.getItem("assignment"));
   }
 
-  selctedImg;
+  selectedImg;
 
   uploadFile(event:any){
     const file:File = event.target.files[0];
-    this.selctedImg = file;
-    console.log('file',this.selctedImg);
+    this.selectedImg = file;
+    this.rep.img = file;
+    console.log('file',this.selectedImg);
+    this.isValid=true;
   }
 
   storeImg(){
-    console.log('file',this.selctedImg.name);
+    console.log('file',this.selectedImg.name);
     const formData = new FormData();
-    formData.append("file", this.selctedImg);
+    formData.append("file", this.selectedImg);
     this.http
-    .post("https://api.ddev.site/movefile.php",formData)
+    .post(environment.apiPath + "/movefile.php",formData)
     .subscribe(
       (res:any)=> { });
   }
+
+  onChange(e, field) {
+    console.log(field);
+    this.rep[field]=e.target.value;
+    console.log(this.rep);
+  }
+
+  doSave() {
+    this.storeImg();
+    this.sqlInsertReport+='("' + this.promoterNo + '", ' +  this.currentAssign['internalPOSNo'] + ', CURDATE(), ' + this.rep.valFrequency + ', ' + this.rep.valLeads + ', ' + this.rep.valTalks +
+      ', ' + this.rep.valTester + ', ' + this.rep.revenue + ', "' + this.rep.display + '", "' + this.rep.nameTag + '", "' + this.rep.tester + '", "' + this.rep.asm + '", "' + this.selectedImg.name + '", ' +
+      '"' + this.rep.comment + '", '+
+      'CURRENT_TIMESTAMP, ' + this.promoterNo + ', CURRENT_TIMESTAMP, ' + this.promoterNo + ')';
+    this.dataService.storeData(this.sqlInsertReport).subscribe(res => { console.log(res) });
+    this.dataStored=true;
+    this.isValid=false;
+  }
+}
+
+const Report = {
+  valFrequency:null,
+  valLeads: null,
+  valTalks: null,
+  valTester: null,
+  revenue: null,
+  display: null,
+  nameTag: null, 
+  tester:null, 
+  asm: null,
+  img: null,
+  comment: null
+
+
 
 }
